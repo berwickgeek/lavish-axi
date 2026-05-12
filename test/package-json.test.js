0ac698092c94c0f7c91011ad58db2ddfe673a680
kunchenguid <kun@kunchenguid.com>
2026-05-11 19:59:06 -0700

    fix: metadata

--- test/package-json.test.js
+++ test/package-json.test.js
@@ -14,3 +14,18 @@ test("check script runs all verification commands", async () => {
     "npm test",
   ]);
 });
+
+test("package metadata matches the GitHub repository used for npm provenance", async () => {
+  const packageJson = JSON.parse(await readFile(new URL("../package.json", import.meta.url), "utf8"));
+
+  assert.equal(packageJson.repository.url, "git+https://github.com/kunchenguid/lavish-axi.git");
+  assert.equal(packageJson.bugs.url, "https://github.com/kunchenguid/lavish-axi/issues");
+  assert.equal(packageJson.homepage, "https://github.com/kunchenguid/lavish-axi#readme");
+});
+
+test("package lock root bin matches the publish manifest", async () => {
+  const packageJson = JSON.parse(await readFile(new URL("../package.json", import.meta.url), "utf8"));
+  const packageLock = JSON.parse(await readFile(new URL("../package-lock.json", import.meta.url), "utf8"));
+
+  assert.deepEqual(packageLock.packages[""].bin, packageJson.bin);
+});
